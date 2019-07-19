f32cc749b2c3fa9d081245f75286da086b34ed2c
Hyunsu Philip Cho <chohyu01@cs.washington.edu>
2019-07-19 04:27:18 +0000

    Use readable logic for endian detection

--- test.cc
+++ test.cc
@@ -1,11 +1,18 @@
 #include <iostream>
 #include <cstdint>
 
+inline int is_system_little_endian() {
+  const uint32_t value = 0x01;
+  const void* address = static_cast<const void*>(&value);
+  const uint8_t* least_significant_address = static_cast<const uint8_t*>(address);
+  return ((*least_significant_address == 0x01) ? 1 : 0);
+}
+
 int main(void) {
-  if ((*(uint16_t *)"\0\xff" < 0x100)) {
-    std::cout << "Big endian" << std::endl;
-  } else {
+  if (is_system_little_endian()) {
     std::cout << "Little endian" << std::endl;
+  } else {
+    std::cout << "Big endian" << std::endl;
   }
   return 0;
 }
